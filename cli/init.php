f820e264333efd14ec0f838f6d382f082be81e39
Joby Harding <joby@iamjoby.com>
2017-04-14 19:28:51 +0100

    Remove 1.0 CLI scripts

--- cli/init.php
+++ cli/init.php
@@ -1,7 +1,7 @@
 <?php
 // This file is part of tool_jasmine
 //
-// Copyright (C) 2016 onwards Joby Harding
+// Copyright (C) 2017 onwards Joby Harding
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright 2016 onwards Joby Harding
+ * @copyright 2017 onwards Joby Harding
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @author    Joby Harding <joby@iamjoby.com>
  * @package   tool_jasmine
@@ -31,35 +31,7 @@ global $CFG;
 define('CLI_SCRIPT', true);
 
 require_once(dirname(dirname(dirname(dirname(__DIR__)))) . '/config.php');
-require_once("$CFG->libdir/clilib.php");
+require_once("{$CFG->libdir}/clilib.php");
 
-echo "Initialising Jasmine spec runners...\n";
-
-$amdmodules = core_requirejs::find_all_amd_modules(true);
-$specdirs = array();
-
-foreach ($amdmodules as $amdmodule) {
-
-    $amddir = dirname(dirname($amdmodule));
-    $specdir = "{$amddir}/" . \tool_jasmine\spec_runner_generator::DIRNAME_SPEC;
-    $specfilepath = "{$amddir}/" . \tool_jasmine\spec_runner_generator::FILENAME_SPECRUNNER;
-
-    // Only once per spec dir.
-    if (is_dir($specdir) && !in_array($specdir, $specdirs)) {
-
-        $specdirs[] = $specdir;
-        $specfilecontent = (new \tool_jasmine\spec_runner_generator())
-            ->add_dir($amddir)
-            ->out();
-
-        echo "\tGenerating {$specfilepath}\n";
-
-        file_put_contents($specfilepath, $specfilecontent);
-        @chmod($specfilepath, 0755);
-
-    }
-
-}
-
-echo "Jasmine spec runner initialisation complete.\n";
-exit(0);
+echo 'TODO';
+exit(1);
